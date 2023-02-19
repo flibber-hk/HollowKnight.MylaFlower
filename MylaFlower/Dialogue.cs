cbd5deab9895ca7a4d53ebd8cfda864a6eb4ce66
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-02-19 07:48:04 +0000

    Add localization support

--- MylaFlower/Dialogue.cs
+++ MylaFlower/Dialogue.cs
@@ -19,13 +19,10 @@ namespace MylaFlower
                 switch (key)
                 {
                     case "FLOWER_OFFER":
-                        return "...Bury my body, stabbed w-with nail<br>Darkness encroaching<br>I wait for Light, and Light approaches<br>A pale, fragile vessel<br>Offered to the shadow within me";
                     case "FLOWER_OFFER_YN":
-                        return "Give the flower?";
                     case "GIVEN_FLOWER":
-                        return "Is this what was calling to me?<br>The aura quiets the voice<br>And breaks through the light and shade";
                     case "NOT_GIVEN_FLOWER":
-                        return "...It is for the best...m-my mind...fractured... can't touch...too pure...";
+                        return Localization.GetText(key);
                 }
 
                 MylaFlower.instance.LogWarn($"Unrecognized key for myla flower sheet: {key}");
@@ -35,14 +32,14 @@ namespace MylaFlower
                 && key == "MINER_DREAM_2" 
                 && MylaFlower.GetMylaState() == MylaState.Crazy)
             {
-                return "C-can I take it ....DESTROY IT.... Is it offered....DON'T TOUCH IT...C-can I take it.... KILL IT....Is it offered.....DANGEROUS";
+                return Localization.GetText("CRAZY_DREAM");
             }
 
             if (sheetTitle == "Minor NPC"
                 && key == "MINER_EARLY_3"
                 && MylaFlower.GetMylaState() == MylaState.Normal)
             {
-                return "H-hello again, my friend! You just missed Schy, although I'm sure they're still here somewhere. Its like that with friends, isn't it?<br>They're always here, e-even when they're not.";
+                return Localization.GetText("GIVEN_DIALOGUE");
                 // return "Oh hello there friend! Look, your present makes a w-wonderful headlamp. Now I can always have it with me. Thank you again!";
             }
 
